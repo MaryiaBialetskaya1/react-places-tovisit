7340e9a5d949720dfe9896d58533bbdbe332b43e
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-15 14:26:51 -0500

    add data to data.js

---
+++ src/data/data.js
@@ -0,0 +1,92 @@
+export const data = [
+  {
+    id: 1,
+    place: "Agra",
+    img: "https://images.unsplash.com/photo-1532664189809-02133fee698d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fFRhaiUyME1haGFsJTIwaW4lMjBBZ3JhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    imgDescription: "Taj Mahal in Agra",
+    placeDescription:
+      "If there was just one symbol to represent all of India, it would be the Taj Mahal. The monument inspires millions of tourists to make the trip to Agra every year, waking up before dawn to see magnificent structure radiate at sunrise. But Agra tops the list of the best places to visit in India for reasons that go beyond India's most famous attraction.",
+    sourceLink: "planetware.com",
+  },
+  {
+    id: 2,
+    place: "New Delhi",
+    img: "https://images.unsplash.com/photo-1646938691456-a03d01a8276a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fEh1bWF5dW4ncyUyMFRvbWJ8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60",
+    imgDescription: "Humayun's Tomb",
+    placeDescription:
+      "Despite its crowds and chaos, New Delhi offers tourists a lot to love. The colorful capital of India is the perfect marriage of heritage and modernity. Old Delhi contains some of the country's most treasured attractions, including the Jama Masjid, Red Fort, and Chandni Chowk shopping thoroughfare. But throughout the sprawling city, tourists can explore countless other sites of spiritual and cultural importance.",
+    sourceLink: "planetware.com",
+  },
+  {
+    id: 3,
+    place: "Mumbai",
+    img: "https://images.unsplash.com/photo-1648466384289-c11b57eab6e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8VGFqJTIwTWFoYWwlMjBQYWxhY2UlMjBIb3RlbCUyMGluJTIwTXVtYmFpfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    imgDescription: "The Taj Mahal Palace Hotel in Mumbai",
+    placeDescription:
+      "Despite its crowds and chaos, New Delhi offers tourists a lot to love. The colorful capital of India is the perfect marriage of heritage and modernity. Old Delhi contains some of the country's most treasured attractions, including the Jama Masjid, Red Fort, and Chandni Chowk shopping thoroughfare. But throughout the sprawling city, tourists can explore countless other sites of spiritual and cultural importance.",
+    sourceLink: "planetware.com",
+  },
+  {
+    id: 4,
+    place: "Rajasthan",
+    img: "https://images.pexels.com/photos/5355764/pexels-photo-5355764.jpeg?auto=compress&cs=tinysrgb&w=800",
+    imgDescription: "Gadi Sagar Temple in Jaisalmer, Rajasthan",
+    placeDescription:
+      "Translated to Land of Kings, Rajasthan brims with remnants of the kings and queens of past centuries. Between its glittering palaces, stately forts, and lively festivals, this western state deserves a starring role in your trip to India.Jaipur, part of the Golden Triangle Tourist Circuit, which also includes Agra and New Delhi, is one of the top places to visit in Rajasthan. Dubbed TheParis of India, it's known for its characteristic pink buildings, lavish City Palace, and jewelry stores galore.",
+    sourceLink: "planetware.com",
+  },
+  {
+    id: 5,
+    place: "Rishikesh",
+    img: "https://images.unsplash.com/photo-1570804485046-b5b8d795dc5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8UmlzaGlrZXNofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    imgDescription: "Rishikesh",
+    placeDescription:
+      "Rishikesh has been on the radar for spiritually minded travelers since the late 1960s, when the Beatles spent time in Maharishi Mahesh Yogi's ashram–now an abandoned site that has become an off-the-beaten-path tourist attraction for fans.The town is nestled in the foothills of the Himalayas on the banks of the holy Ganges River, and serves as a center for yoga and pilgrimages. Take part in the action, or just enjoy the sounds of the temple bells and sightseeing from Rishikesh's two suspension bridges, often guarded by assertive families of monkeys. Keep your distance.",
+    sourceLink: "planetware.com",
+  },
+  {
+    id: 6,
+    place: "Amritsar",
+    img: "https://images.unsplash.com/photo-1517427677506-ade074eb1432?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8QW1yaXRzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    imgDescription: "The Golden Temple in Amritsar",
+    placeDescription:
+      "Amritsar, the Jewel of Punjab, has made its claim to fame with its remarkable Golden Temple. One of the holiest places in the world for Sikhs, the gilded structure is a sight to behold, glistening in the sun and reflecting into the large pool that surrounds it.The attraction also boasts the world's largest community kitchen, which serves 100,000 diners (including curious tourists!) lentils and curries every day.While in Amritsar, plan to spend an afternoon at the border of Pakistan to see the Beating Retreat Ceremony. Goose-stepping guards from long-term rivals India and Pakistan open and close the border gates at dusk in an over-the-top ceremony you'll never forget. Get there early to dance to blaring Bollywood music with locals in the streets.",
+    sourceLink: "planetware.com",
+  },
+  {
+    id: 7,
+    place: "Goa",
+    img: "https://images.unsplash.com/photo-1614082242765-7c98ca0f3df3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8UGFsb2xlbSUyMEJlYWNoJTJDJTIwU291dGglMjBHb2F8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    imgDescription: "Palolem Beach, South Goa",
+    placeDescription:
+      "India's not just full of big cities and holy sites–it also has incredible beaches down south in Goa. Its stretches of golden sand along the Arabian Sea offer something for every type of tourist, whether you're interested in hanging out with the backpacker crowd in laid-back beach huts or having a ritzy tropical getaway at a five-star resort.One unique part of Goa is its blend of Indian and Portuguese cultures. You'll experience the fusion throughout the destination, from its Baroque architecture and cathedrals to its spicy vindaloo curries and seafood dishes.",
+    sourceLink: "planetware.com",
+  },
+  {
+    id: 8,
+    place: "Kerala",
+    img: "https://images.unsplash.com/photo-1593693401060-9fc28cf9e368?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8SG91c2Vib2F0JTIwaW4lMjBLZXJhbGF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+    imgDescription: "Houseboat in Kerala",
+    placeDescription:
+      "Head south of Goa, and you'll trade beaches for tranquil backwaters in Kerala. Nothing beats the experience of hopping aboard a traditional thatched-top houseboat in Alleppey (also known as Alappuzha) and slowly floating through palm-fringed lagoons and rivers, either as a day trip or overnight adventure. You'll enjoy freshly cooked Indian cuisine on the water and breathtakingly beautiful natural sights and wildlife.Easygoing Kerala is like a breath of fresh air from the intensity of cities like New Delhi and Jaipur up north. Schedule some time here when you're in need of a break from the chaos.",
+    sourceLink: "planetware.com",
+  },
+  {
+    id: 9,
+    place: "Shimla",
+    img: "https://images.unsplash.com/photo-1597074866923-dc0589150358?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8U2hpbWxhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    imgDescription: "Shimla",
+    placeDescription:
+      "When the temperatures skyrocket in New Delhi and other cities in North India, tourists and locals alike make their way to cooler climates in the hill stations, the most popular of which is Shimla. The cloudy weather and forested hillsides make for a satisfying escape from the heat, as well as a placid place to spend a weekend or more. The atmosphere in the hilly central part of town, where traffic is banned, is just as pleasant as the scenic outskirts. While you're in the area, make a reservation on the Kalka-Shimla Railway, a UNESCO World Heritage Site. The 96.6-kilometer-long railroad, which has been running for over a century, is renowned for its stunning views and authentic vintage experience.",
+    sourceLink: "planetware.com",
+  },
+  {
+    id: 10,
+    place: "Ladakh",
+    img: "https://images.unsplash.com/photo-1658077589571-3db75c68e53d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8TGFrZSUyMGFuZCUyMGJlYXV0aWZ1bCUyMG1vdW50YWluJTIwc2NlbmVyeSUyMGluJTIwTGFkYWtofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    imgDescription: "Lake and beautiful mountain scenery in Ladakh",
+    placeDescription:
+      "Situated amid the jagged crags of the Karakoram and Zanskar mountain ranges, Ladakh offers tourists the chance to immerse themselves in an awe-inspiring alpine desert. Driving around the region's winding roads comes with one incredible natural view after the next: plunging valleys, gushing rivers, snowcapped peaks, and more. Ladakh's enchanting capital, Leh, stands at 3,500 meters above sea level, so you'll want to spend at least a few days acclimating to the altitude before embarking on any strenuous activities.",
+    sourceLink: "planetware.com",
+  },
+];
