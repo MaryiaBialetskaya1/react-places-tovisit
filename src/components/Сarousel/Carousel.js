da3b777b48c1ba8cbff5e016ac9f4a49299aabc1
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-15 20:04:05 -0500

    add logic to buttons

--- src/components/Сarousel/Carousel.js
+++ src/components/Сarousel/Carousel.js
@@ -6,10 +6,29 @@ export function Carousel() {
   const [images, setImages] = useState(0);
   const { img, imgDescription } = data[images];
 
+  const showPreviousImage = () => {
+    setImages((images) => {
+      images--;
+      if (images < 0) {
+        return data.length - 1;
+      }
+      return images;
+    });
+  };
+
+  const showNextImage = () => {
+    setImages((images) => {
+      images++;
+      if (images > data.length - 1) {
+        images = 0;
+      }
+      return images;
+    });
+  };
   return (
     <div className={s.container}>
       <div>
-        <button>&larr;</button>
+        <button onClick={showPreviousImage}>&lArr;</button>
       </div>
       <div>
         <div>
@@ -20,7 +39,7 @@ export function Carousel() {
         </div>
       </div>
       <div>
-        <button>&rarr;</button>
+        <button onClick={showNextImage}>&rArr;</button>
       </div>
     </div>
   );
